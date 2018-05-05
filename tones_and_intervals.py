6446569219c0a3d296f9c1c67b6422b946561b74
jasonflatley <jasonflatley@gmail.com>
2018-05-04 18:33:43 -0700

    initial commit 2

---
+++ tones_and_intervals.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  4 17:59:51 2018
+
+@author: jasonflatley
+"""
+
+class pitch:
+    """
+    docstring
+    """
+    
+    def __init__(self, pitch):
+        """
+        A pitch is an integer in [0, 11] that represents one of the
+        western twelv
+        
+        self._pitch = pitch
+        self._harmonic_env = harmonic_env
