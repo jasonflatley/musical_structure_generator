c81afd965e6c5217324d0809d46b326ca26932f4
jasonflatley <jasonflatley@gmail.com>
2018-05-09 16:01:28 -0700

    implemented interval class

--- tones_and_intervals.py
+++ tones_and_intervals.py
@@ -1,41 +1,68 @@
 
 
 """
-This code implements music.
+This code implements selected parts of Western diatonic music theory.
 """
 
 import re
 
 
-# Relates interval names with number of semi-tones
-# We will use code later to modify it for higher octaves
-distances = {'p1': 0,
-             'a1': 1,
-             'd2': 1,
-             'p2': 2,
-             'a2': 3,
-             'd3': 3,
-             'p3': 4,
-             'd4': 4,
-             'a3': 5,
-             'p4': 5,
-             'a4': 6,
-             'd5': 6,
-             'p5': 7,
-             'a5': 8,
-             'd6': 8,
-             'p6': 9,
-             'a6': 10,
-             'd7': 10,
-             'p7': 11,
-             'd8': 11,
-             'p8': 12
-             }
+# Translates diatonic intervals within one octave into the corresponding number of semi-tones
+diatonic_to_num_semitones = {'p1': 0,
+                              'a1': 1,
+                              'd2': 1,
+                              'p2': 2,
+                              'a2': 3,
+                              'd3': 3,
+                              'p3': 4,
+                              'd4': 4,
+                              'a3': 5,
+                              'p4': 5,
+                              'a4': 6,
+                              'd5': 6,
+                              'p5': 7,
+                              'a5': 8,
+                              'd6': 8,
+                              'p6': 9,
+                              'a6': 10,
+                              'd7': 10,
+                              'p7': 11,
+                              'd8': 11}
+
+# Translates a number of semi-tones into a corresponding perfect diatonic interval
+num_semitones_to_perfect = {0: 'p1',
+                            2: 'p2',
+                            4: 'p3',
+                            5: 'p4',
+                            7: 'p5',
+                            9: 'p6',
+                            11: 'p7'}
+
+# Translates a number of semi-tones into a corresponding augmented diatonic interval
+num_semitones_to_augmented = {1: 'a1',
+                              3: 'a2',
+                              5: 'a3',
+                              6: 'a4',
+                              8: 'a5',
+                              10: 'a6'}
+
+# Translates a number of semi-tones into a corresponding diminished diatonic inteval
+num_semitones_to_diminished = {1: 'd2',
+                               3: 'd3',
+                               4: 'd4',
+                               6: 'd5',
+                               8: 'd6',
+                               10: 'd7',
+                               11: 'd8'}
+
+
+# Letter names of diatonic notes in order
+letter_names = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
 
 
 class interval:
     """
-    This class implements a musical interval
+    This class implements the concept of a musical interval in diatonic Western music
     """
 
     
@@ -44,152 +71,152 @@ class interval:
         Class to implement musical intervals.
         
         Perfect interval categories (fundamental plus octave shifted):
-            Unison: p1
+            Unison/Octave: p1, p8, p15, p22 (1 + 7n for n >= 0)      
             Second: p2, p9, p16, etc. (2 + 7n for n >= 0)
             Third: p3, p10, p17, etc. (3 + 7n for n >= 0)
             Fourth: p4, p11, p18, etc. (4 + 7n for n >= 0)
             Fifth: p5, p12, p19, etc. (5 + 7n for n >= 0)
             Sixth: p6, p13, p20, etc. (6 + 7n for n >= 0)
             Seventh: p7, p14, p21 etc. (7 + 7n for n >= 0)
-            Octave: p8, p15, p22, etc. (8 + 7n for n >= 1)
-            
-            So we can have perfect intervals of form p + [any integer >= 1] 
-            
-        Augmentable intervals: a1, a2, a3, a4, a5, a6 and octave shifts
-        Diminishable intervals: d2, d3, d4, d5, d6, d7, d8 and octave shifts
+
+            General: reduced_interval_number + 7*octave_offset
+
+        Augmentable intervals: a1, a2, a3, a4, a5, a6 and further octave shifts
+        Diminishable intervals: d2, d3, d4, d5, d6, d7, d8 and further octave shifts
+        Basically, d1 doesn't exist, and a7, a14, a21 don't exist
         """
         
+        self._interval_name = interval_name        
+        self._interval_type = self._interval_name[0]
+        self._interval_number = int(self._interval_name[1:])
+        
         # Validate interval_name input
-        if (not re.match(r'[adp]\d+\b', interval_name) or interval_name == 'd1'):
+        # Has to be a string with a, d, or p followed by a number, and can't be d1 or a7, a14, a21, etc
+        if (not re.match(r'[adp]\d+\b', self._interval_name) 
+        or self._interval_name == 'd1'
+        or (self._interval_type == 'a' and self._interval_number % 7 == 0)):
             raise ValueError('Invalid interval name specified')
             
-        self._interval_name = interval_name
+        # Compute the equivalent diatonic interval name in one octave
+        # We use mod 7 arithemtic, but we start sevenths with 7 instead of 0
+        # (There is no diatonic '0' relationship)
+        self._reduced_interval_number = self._interval_number % 7
+        if self._reduced_interval_number == 0:
+            self._reduced_interval_number = 7
+            
+        # Form the diatonic name of the reduced interval
+        # Also compute the number of half steps in it as base_length
+        self._reduced_interval_name = self._interval_type + str(self._reduced_interval_number)
+        self._base_length = diatonic_to_num_semitones[self._reduced_interval_name]
+            
+        # Compute how many octaves above the first that our interval lies in
+        self._octave_offset = int((self._interval_number - self._reduced_interval_number)/7)
+                 
+    
+    def semitones_to_diasteps(num_semitones, interval_type):
+        """
+        Take a given number of semitones and translate it into the corresponding
+        type of diatonic interval
+        TODO: check this for invalid input?
+        """
+        base_num_semitones = num_semitones % 12
+        octave_offset = num_semitones//12
         
+        if interval_type == 'p':
+            base_diatonic_interval = num_semitones_to_perfect[base_num_semitones]
+        elif interval_type == 'a':
+            base_diatonic_interval = num_semitones_to_augmented[base_num_semitones]
+        elif interval_type == 'd':
+            base_diatonic_interval = num_semitones_to_diminished[base_num_semitones]
+            
+        new_interval_number = int(base_diatonic_interval[1:]) + 7*octave_offset
+        
+        return interval(interval_type + str(new_interval_number))
+
     
     def __len__(self):
         """
-        Use our distance dictionary to figure out how many semitones
-        a given interval represents
+        Return the number of semitones in the given interval
+        This could also be called diasteps_to_semitones in comparison with the
+        previous function.
         """
-        if self._interval_name == 'p1':
-            return distances['p1']
-        else:
-            # the number in the interval is x + 7n for some x we need to find
-            # use mod 7, but handle exceptions for sevenths and octaves
-            interval_type = self._interval_name[0]
-            interval_number = int(self._interval_name[1:])
-            reduced_interval_number = interval_number % 7
-            
-            if reduced_interval_number == 0:
-                reduced_interval_number = 7
-            elif reduced_interval_number == 1:
-                reduced_interval_number = 8
-            
-            reduced_interval_name = self._interval_name[0] + str(reduced_interval_number)
-            
-            base_length = distances[reduced_interval_name]
-            
-            # every additional 7 beyond base_length  in interval_number adds an octave, or 12 semitones
-            octave_offset = 12*(interval_number - base_length)/7
-            
-            # augment or diminish the interval if necessary
-            if interval_type == 'a':
-                a_d_offset = 1
-            elif interval_type == 'd':
-                a_d_offset = -1
-            else:
-                a_d_offset = 0
-                
-            return base_length + octave_offset + a_d_offset
+        return self._base_length + 12*self._octave_offset
+    
+    
+    def run(self):
+        """
+        TODO: figure out how to get the desired output when we call an object
+        """
+
+        return self._interval_name
+    
+    def __eq__(self, other):
+        return self._interval_name == other._interval_name
+    
+    def __ne__(self, other):
+        return not self == other
     
         
     def __add__(self, other):
-        print(distances['p1'])
+        """
+        Figure out what happens when we add intervals together. 
+        There are a few cases:
+            perfect + perfect = perfect (unless adding perfect 4ths (up to octave shifts), then it's diminished)
+            augmented + augmented = perfect, then augmented
+            diminished + diminished = perfect, then diminished
+            augmented + diminished = perfect, then diminshed (the second one)
+            diminished + augmented = perfect, then augmented (the second one)  
+        
+        Strategy:
+            Add the two lengths to get the number of semitones
+            Divide this number into the form reduced_interval_number + 7*octave_offset
+            Figure out what type of interval the result should be
+            Look up reduced_interval_number in the dictionary
+        """
+        
+        # Add the lengths of the two intervals to get the total number of semitones
+        # in the new interval
+        new_num_semitones = len(self) + len(other)
+        
+        # Implement the addition rules above
+        if self._reduced_interval_name == 'p4' and other._reduced_interval_name == 'p4':
+            final_interval_type = 'd'
+        elif self._interval_type == 'p' and self._interval_type == 'p':
+            final_interval_type = 'p'
+        elif self._interval_type == 'a' and other._interval_type == 'a':
+            try:
+                final_interval_type = 'p'
+            except:
+                final_interval_type = 'a'
+        elif self._interval_type == 'd' and other._interval_type == 'd':
+            try:
+                final_interval_type = 'p'
+            except:
+                final_interval_type = 'd'
+        elif self._interval_type == 'a' and other._interval_type == 'd':
+            try:
+                final_interval_type = 'p'
+            except:
+                final_interval_type = 'd' 
+        elif self._interval_type == 'd' and other._interval_type == 'a':
+            try:
+                final_interval_type = 'p'
+            except:
+                final_interval_type = 'a' 
+        
+        return self.semitones_to_diasteps(new_num_semitones, final_interval_type)
+            
 
+"""
+TODO: interval regresion tests
 
-
-
-a = interval('p22')
-
+a = interval('p3')
+b = interval('p4')
 len(a)
 
-asdf = 'p22'
-int(asdf[1:])%7
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if not re.match(r'[adp]\d+\b', 'd23489'):
-    print('hey!')
-else:
-    print('NOOOOOOOOOOO')
-
-
-
-
-
-def transpose(note_num, interval, direction):
-    """
-    Transpose note_num by the number of semi-tones indicated by interval
-    Shit, we need to be able to do interval arithmetic
-    Or do we?
-    perfect + perfect = perfect (unless adding perfect 4ths, then it's diminished)
-    augmented + augmented = perfect, then augmented
-    diminished + diminished = perfect, then diminished
-    augmented + diminished = perfect, then diminshed (the second one)
-    diminished + augmented = perfect, then augmented (the second one)
-    so we need an INTERVAL CLASS! with __add__
-    
-    """
-    
-    
-    
-    return note_num
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+c = a + b
+c._interval_name
+"""
 
 
 
@@ -198,36 +225,99 @@ def transpose(note_num, interval, direction):
 class pitch:
     
     """
-    Class: pitch
     A pitch is one of the Western 12 tones plus which octave it is in.
     It corresponds to a key on the piano (A0-C8, C4 is middle C), plus 
     the correct enharmonic name.
     The note name is specified in lilypond notation for ease of notation later.
+    We represent a pitch as a root plus an interval for enharmonic purposes
     
-    Fields:
-    _note_num - integer distance from C1 = 0, C4 = middle C = 48 on a piano keyboard, the unique identifier
-    _tonal_center - tonal center of the harmonic environment we have in mind
-    _harmonic_env - harmonic environment: ionian, lydian, dorian, etc.
-    _note_name - enharmonically correct name in lilypond notation
+    So in B major, with B = 47, the third is 51, how do we know that's D# not Eb?
+    pitch = 51
+    tonal_center = B
+    harmonic_env = major
+    interval_from_root = P3
+    So since it's a third away from B, it has to be some kind of D and not some kind of E
     
-    Behaviors:
-    transpose(note, interval)    
+    so like C# in A dorian can be a #17
+    A C E G B D F# A C# E G# B D# F# 
+    E in C dorian is a "#17" it's Eb#
+    C Eb G Bb D F A C E G
+
     """
     
-    def __init__(self, note_num, tonal_center, interval_from_root):
+    def find_note_name(note_num, direction):
         """
-        docstring
+        Quick helper function to return a lilypond note name for a given note_num
         """
         
-        self._note_num = note_num
-        self._tonal_center = tonal_center
+        if direction == 'neutral':
+            find_note_name_dict = {0: 'c',
+                                   2: 'd',
+                                   4: 'e',
+                                   5: 'f',
+                                   7: 'g',
+                                   9: 'a',
+                                   11: 'b'}
+        elif direction == 'sharp':
+            find_note_name_dict = {0: 'c',
+                                   1: 'cis',
+                                   2: 'd',
+                                   3: 'dis',
+                                   4: 'e',
+                                   5: 'f',
+                                   6: 'fis',
+                                   7: 'g',
+                                   8: 'gis',
+                                   9: 'a',
+                                   10: 'ais',
+                                   11: 'b'}
+        elif direction == 'flat':
+            find_note_name_dict = {0: 'c',
+                                   1: 'des',
+                                   2: 'd',
+                                   3: 'ees',
+                                   4: 'e',
+                                   5: 'f',
+                                   6: 'ges',
+                                   7: 'g',
+                                   8: 'aes',
+                                   9: 'a',
+                                   10: 'bes',
+                                   11: 'b'}
+        
+        return find_note_name_dict[note_num % 12]
+            
+
+
+        
+    
+    def __init__(self, root_note_num, root_note_name, interval_from_root):
+        """
+        root_note_num is a number from -3 to X representing the number of key on piano
+        root_note_name is one of the possible names for the root note, such as cisis
+        interval_from_root is how far the actual pitch is from the root note
+        """
+        
+        # Check that root_note_num and root_note_name are consistent
+        count_sharp = root_note_name.count('is')
+        count_flat = root_note_name.count('es')
+        
+        if count_sharp == 0 and count_flat == 0:
+            find_note_name[]
+        
+        
+        # Initialize attributes
+        self._root_note_num = root_note_num
+        self._root_note_name = root_note_name
         self._interval_from_root = interval_from_root
+        self._note_name = self.get_note_name(self)
+        
         
     def get_note_name(self):
         """
         Returns the name of the pitch in lilypond notation
         """
-        letter_names = ['c', 'd', 'e', 'f', 'g', 'a', 'b']
+        
         interval_type = self._interval_from_root[0]
         interval_distance = self._interval_from_root[1]
         new_letter_name = letter_names[(int(letter_names.index(self._tonal_center)) + int(interval_distance) - 1)%7]
@@ -237,82 +327,21 @@ class pitch:
             return new_letter_name + 'is'
         elif interval_type == 'd':
             return new_letter_name + 'es'
-            
+        
+    def transpose(note_num, interval, direction):
+        """
+        Transpose note_num by the number of semi-tones indicated by interval
+        Shit, we need to be able to do interval arithmetic
+        """
+    
+
+
+a = 'deses'
+a.count('es')
+
+
 
 
 """
-C: P1,
-C#, Db: A1, D2
-D: P2
-D#, Eb: A2, D3
-E, Fb: P3, D4
-E#, F: A3, P4
-F#, Gb: A4, D5
-G: P5
-G#, Ab: A5, D6
-A: P6
-A#, Bb: A6, D7
-B: P7, D8
------
-C: P8
-C#, Db: A8, D9
-...
+classes: scale
 """
-
-
-
-
-
-
-
-def get_note_name(root_note_name, root_note_num, interval_from_root):
-    """
-    Returns the name of the pitch in lilypond notation
-    """
-
-    letter_names = ['c', 'd', 'e', 'f', 'g', 'a', 'b']
-    
-    interval_type = interval_from_root[0]
-    interval_distance = int(interval_from_root[1])
-    
-    new_letter_num = (letter_names.index(root_note_name) + interval_distance - 1)%7
-    new_letter_name = letter_names[new_letter_num]
-    
-    if interval_type == 'p':
-        return new_letter_name
-    elif interval_type == 'a':
-        return new_letter_name + 'is'
-    elif interval_type == 'd':
-        return new_letter_name + 'es'
-
-get_note_name('b', 47, 'p3')
-
-
-
-
-So in B major, with B = 47, the third is 51, how do we know that's D# not Eb?
-pitch = 51
-tonal_center = B
-harmonic_env = major
-interval_from_root = P3
-So since it's a third away from B, it has to be some kind of D and not some kind of E
-
-# so like C# in A dorian can be a #17
-# A C E G B D F# A C# E G# B D# F# 
-# E in C dorian is a "#17" it's Eb#
-# C Eb G Bb D F A C E G
-
-"""       
- 
-
-
-
-
-
-
-
-
-
-
-       
-    
