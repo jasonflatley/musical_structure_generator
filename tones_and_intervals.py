83705212120cb52b5592bfa13334ba942854a612
jasonflatley <jasonflatley@gmail.com>
2018-05-18 18:22:07 -0700

    starting with scale class

--- tones_and_intervals.py
+++ tones_and_intervals.py
@@ -60,6 +60,8 @@ num_semitones_to_diminished = {1: 'd2',
 letter_names = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
 
 
+
+
 class interval:
     """
     This class implements the concept of a musical interval in diatonic Western music
@@ -70,10 +72,13 @@ class interval:
         """
         Class to implement musical intervals.
         
+        The interval_name paramter is something like p4, a5 or d6 to represent
+        perfect fourths, augmented fifths, diminished sixths and such like
+        
         Perfect interval categories (fundamental plus octave shifted):
-            Unison/Octave: p1, p8, p15, p22 (1 + 7n for n >= 0)      
-            Second: p2, p9, p16, etc. (2 + 7n for n >= 0)
-            Third: p3, p10, p17, etc. (3 + 7n for n >= 0)
+            Unison/Octave/Fifteenth: p1, p8, p15, etc. (1 + 7n for n >= 0)      
+            Second/Ninth/Sixteenth: p2, p9, p16, etc. (2 + 7n for n >= 0)
+            Third/etc.: p3, p10, p17, etc. (3 + 7n for n >= 0)
             Fourth: p4, p11, p18, etc. (4 + 7n for n >= 0)
             Fifth: p5, p12, p19, etc. (5 + 7n for n >= 0)
             Sixth: p6, p13, p20, etc. (6 + 7n for n >= 0)
@@ -86,48 +91,77 @@ class interval:
         Basically, d1 doesn't exist, and a7, a14, a21 don't exist
         """
         
+        # _interval_type and _interval_number break up _interval_name = 'p5' into
+        # _interval_type = 'p' and _interval_number = 5
         self._interval_name = interval_name        
         self._interval_type = self._interval_name[0]
         self._interval_number = int(self._interval_name[1:])
         
         # Validate interval_name input
-        # Has to be a string with a, d, or p followed by a number, and can't be d1 or a7, a14, a21, etc
-        if (not re.match(r'[adp]\d+\b', self._interval_name) 
+        # Has to be a string with a, d, or p followed by an integer > 1, 
+        # and can't be d1 or a7, a14, a21, etc
+        if (not re.match(r'[adp]\d+\b', self._interval_name)
+        or type(self._interval_number) != int
+        or self._interval_number < 1
         or self._interval_name == 'd1'
         or (self._interval_type == 'a' and self._interval_number % 7 == 0)):
             raise ValueError('Invalid interval name specified')
             
         # Compute the equivalent diatonic interval name in one octave
         # We use mod 7 arithemtic, but we start sevenths with 7 instead of 0
-        # (There is no diatonic '0' relationship)
+        # (since here is no diatonic '0' relationship)
+        # This way, we know that a perfect 17th is just a perfect third plus two octaves
         self._reduced_interval_number = self._interval_number % 7
         if self._reduced_interval_number == 0:
             self._reduced_interval_number = 7
             
         # Form the diatonic name of the reduced interval
         # Also compute the number of half steps in it as base_length
+        # So for a perfect 17th, _reduced_interval_name = 'p3' and _base_length = 4
         self._reduced_interval_name = self._interval_type + str(self._reduced_interval_number)
         self._base_length = diatonic_to_num_semitones[self._reduced_interval_name]
             
         # Compute how many octaves above the first that our interval lies in
+        # So for a perfect 17th, _octave_offset = 2
         self._octave_offset = int((self._interval_number - self._reduced_interval_number)/7)
                  
     
     def semitones_to_diasteps(num_semitones, interval_type):
         """
         Take a given number of semitones and translate it into the corresponding
-        type of diatonic interval
-        TODO: check this for invalid input?
+        type of diatonic interval.
+        
+        Examples:
+        num_semitones = 4 and interval_type = 'p' returns 'p3'
+        num_semitones = 4 and interval_type = 'd' returns 'd4'
+        num_semitones = 4 and interval_type = 'a' gives an error because there is no such thing
         """
+        
+        if (not re.match(r'[adp]\d+\b', self._interval_name)
+        or type(self._interval_number) != int
+        or self._interval_number < 1
+        or self._interval_name == 'd1'
+        or (self._interval_type == 'a' and self._interval_number % 7 == 0)):
+            raise ValueError('Invalid interval name')
+        
         base_num_semitones = num_semitones % 12
         octave_offset = num_semitones//12
-        
+                            
         if interval_type == 'p':
-            base_diatonic_interval = num_semitones_to_perfect[base_num_semitones]
+            if base_num_semitones in num_semitones_to_perfect:
+                base_diatonic_interval = num_semitones_to_perfect[base_num_semitones]
+            else:
+                raise ValueError('Invalid interval relationship')
         elif interval_type == 'a':
-            base_diatonic_interval = num_semitones_to_augmented[base_num_semitones]
+            if base_num_semitones in num_semitones_to_augmented:
+                base_diatonic_interval = num_semitones_to_augmented[base_num_semitones]
+            else:
+                raise ValueError('Invalid interval relationship')
         elif interval_type == 'd':
-            base_diatonic_interval = num_semitones_to_diminished[base_num_semitones]
+            if base_num_semitones in num_semitones_to_diminished:
+                base_diatonic_interval = num_semitones_to_diminished[base_num_semitones]
+            else:
+                raise ValueError('Invalid interval relationship')
             
         new_interval_number = int(base_diatonic_interval[1:]) + 7*octave_offset
         
@@ -143,11 +177,11 @@ class interval:
         return self._base_length + 12*self._octave_offset
     
     
-    def run(self):
+    def __repr__(self):
         """
-        TODO: figure out how to get the desired output when we call an object
+        Print the name of the interval when we run one of these objects
+        https://stackoverflow.com/questions/12412324/python-class-returning-value
         """
-
         return self._interval_name
     
     def __eq__(self, other):
@@ -160,7 +194,8 @@ class interval:
     def __add__(self, other):
         """
         Figure out what happens when we add intervals together. 
-        There are a few cases:
+        
+        There are a few cases. We may have to strongly update this logic later.
             perfect + perfect = perfect (unless adding perfect 4ths (up to octave shifts), then it's diminished)
             augmented + augmented = perfect, then augmented
             diminished + diminished = perfect, then diminished
@@ -211,7 +246,8 @@ class interval:
 TODO: interval regresion tests
 
 a = interval('p3')
-b = interval('p4')
+b = interval('p3')
+a==b
 len(a)
 
 c = a + b
@@ -222,16 +258,62 @@ c._interval_name
 
 
 
+
+class scale:
+    """
+    A scale is an ordered collection of ascending intervals starting with a unison (or 'p1' 
+    in our interval notation), wh
+    
+    
+    For example:    
+    Ionian scale: ['p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7']
+    Dorian scale: ['p1', 'p2', 'm3', 'p4', 'p5', 'p6', 'm7']
+
+
+    """
+
+
+
+
+
+
+
+
+
+
+
 class pitch:
     
     """
-    A pitch is one of the Western 12 tones plus which octave it is in.
-    It corresponds to a key on the piano (A0-C8, C4 is middle C), plus 
-    the correct enharmonic name.
-    The note name is specified in lilypond notation for ease of notation later.
-    We represent a pitch as a root plus an interval for enharmonic purposes
+    For this system, a pitch corresponds to a key on a theoretically infinite piano. For practical
+    purposes, most of the stuff we do takes place on the 88 actual piano keys.
+
+    There are several important representations for each pitch. We'll use middle C as an example.
+
+    There is only one unique representation (i.e., no enharmonic or other equivalents):
+    note_number_rep: 48 (the 48th key from the left on a piano)
     
-    So in B major, with B = 47, the third is 51, how do we know that's D# not Eb?
+    The rest of the representations admit of various enharmonic spellings:
+    tonal_center_rep: tonal_center = 'aes', interval_from_root = 'p3', harmonic_env = 'major'
+    piano_key_rep: "C4" (the fourth C from the bottom)
+    lilypond_rep: "c'" (one octave above C in the middle of bass clef)
+
+    or:
+    tonal_center_rep: tonal_center = 'gis', interval_from_root = 'p3', harmonic_env = 'major'
+    piano_key_rep: "B#4" (the fourth B# from the bottom)
+    lilypond_rep: "bis" (within one octave above C in the middle of bass clef)
+    
+    or:
+    tonal_center_rep: tonal_center = 'ces', interval_from_root = 'd2', harmonic_env = 'major'
+    piano_key_rep: "Dbb4" (the fourth Dbb from the bottom)
+    lilypond_rep: "deses'" (one octave above C in the middle of bass clef)
+    
+    Other possibilities
+    sargam_rep: tonal_center = 'aes', sargam_from_root = 'ga'
+    frequency_rep (something about Easley Blackwood tunings and such)
+    
+    ---------
+    So in B major, for example with B = 47, the third is 51, how do we know it should be D# and not Eb?
     pitch = 51
     tonal_center = B
     harmonic_env = major
@@ -348,10 +430,15 @@ a.count('es')
 
 
 """
-classes: scale
+
 map to frequencies
 mirror exercises
 negative harmony 
 scales with tetrachords
 harmonica the quadruple flat 3 is just the #4
+
+indian music frequencies and just intonation
+
+define arbitrary microtonal "diatonic" systems
+
 """
